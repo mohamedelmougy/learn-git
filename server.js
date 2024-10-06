b35f7bc63d5a75ca1f7f6c929e1e387fb302640a
Mohamed-Elmougy11 <mohamed.elmougy11@gmail.com>
2024-10-06 22:04:22 +0300

    version34

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ console.log("second change")
 console.log("second change")
 console.log("second change")
 console.log("second change")
-console.log("second change")
-console.log("second change")
 const log = (function () {
     return function () {
         return console.log("hello world")
